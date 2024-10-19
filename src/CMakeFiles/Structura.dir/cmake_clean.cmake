59e335f141dc9775c4db9edcb26342431037f4c8
Chessy <ilovepakpak@gmail.com>
2024-10-19 23:56:30 +0700

    Cmake , CICD update

---
+++ src/CMakeFiles/Structura.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Structura.dir/main.cpp.o"
+  "CMakeFiles/Structura.dir/main.cpp.o.d"
+  "Structura"
+  "Structura.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Structura.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/CMakeFiles/Structura.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Structura.
+# This may be replaced when dependencies are built.
